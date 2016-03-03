19d117d7a9e283284a23378af5a4db33e6993d6a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-03 20:41:23 +0700

    tambah user event handler

---
+++ Server/src/main/kotlin/com/rizki/mufrizal/aplikasi/helpdesk/labti/repository/eventHandler/UserRepositoryEventHandler.kt
@@ -0,0 +1,35 @@
+package com.rizki.mufrizal.aplikasi.helpdesk.labti.repository.eventHandler
+
+import com.rizki.mufrizal.aplikasi.helpdesk.labti.domain.User
+import com.rizki.mufrizal.aplikasi.helpdesk.labti.domain.UserRole
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
+import java.util.*
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since 03 March 2016, 18:35
+ * @Project Server
+ * @Package com.rizki.mufrizal.aplikasi.helpdesk.labti.repository.eventHandler
+ */
+
+@Component
+@RepositoryEventHandler(User::class)
+class UserRepositoryEventHandler {
+
+    @HandleBeforeCreate
+    fun handleUserBeforeCreate(user: User) {
+        val bCryptPasswordEncoder = BCryptPasswordEncoder()
+        user.password = bCryptPasswordEncoder.encode(user.password)
+
+        val userRole = UserRole()
+        userRole.user = user
+
+        val userRoles: Set<UserRole> = HashSet()
+        userRoles.plus(userRole)
+        user.userRoles = userRoles
+    }
+
+}
