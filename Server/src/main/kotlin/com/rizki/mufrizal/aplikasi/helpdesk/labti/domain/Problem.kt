642955bde7d0cb34c58b936e81cdcbd989ec5120
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-03 21:40:25 +0700

    tambah model problem

---
+++ Server/src/main/kotlin/com/rizki/mufrizal/aplikasi/helpdesk/labti/domain/Problem.kt
@@ -0,0 +1,49 @@
+package com.rizki.mufrizal.aplikasi.helpdesk.labti.domain
+
+import org.hibernate.annotations.GenericGenerator
+import java.io.Serializable
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Lob
+import javax.persistence.Table
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since 03 March 2016, 21:32
+ * @Project Server
+ * @Package com.rizki.mufrizal.aplikasi.helpdesk.labti.domain
+ */
+
+@Entity
+@Table(name = "tb_problem")
+class Problem : Serializable {
+
+    @Id
+    @Column(name = "id_problem", length = 150)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    var idProblem: String? = null
+
+    @Column(name = "judul", length = 50)
+    var judul: String? = null
+
+    @Lob
+    @Column(name = "deskripsi")
+    var deskripsi: String? = null
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    var status: Status? = null
+
+    @Column(name = "tanggalProblem")
+    @Temporal(TemporalType.TIMESTAMP)
+    var tanggalProble: Date = Date()
+
+}
