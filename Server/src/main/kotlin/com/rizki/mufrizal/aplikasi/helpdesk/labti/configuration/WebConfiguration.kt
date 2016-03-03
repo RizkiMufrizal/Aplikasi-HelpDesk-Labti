5eba90dc1fc406143591ab3299fec985c49a0b07
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-03 20:41:42 +0700

    tambah konfigurasi web

---
+++ Server/src/main/kotlin/com/rizki/mufrizal/aplikasi/helpdesk/labti/configuration/WebConfiguration.kt
@@ -0,0 +1,22 @@
+package com.rizki.mufrizal.aplikasi.helpdesk.labti.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since 01 March 2016, 15:27
+ * @Project Server
+ * @Package com.rizki.mufrizal.aplikasi.helpdesk.labti.configuration
+ */
+
+@Configuration
+open class WebConfiguration : WebMvcConfigurerAdapter() {
+
+    override fun addViewControllers(viewControllerRegistry: ViewControllerRegistry) {
+        viewControllerRegistry.addViewController("/").setViewName("index")
+        viewControllerRegistry.addViewController("/login").setViewName("login")
+    }
+
+}
