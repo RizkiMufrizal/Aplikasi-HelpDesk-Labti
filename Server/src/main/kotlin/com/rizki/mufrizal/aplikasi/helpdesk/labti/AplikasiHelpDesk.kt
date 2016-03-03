29d037928e2c23601d3638c0697a4770de592aa7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-03 20:39:19 +0700

    tambah root aplikasi untuk spring boot

---
+++ Server/src/main/kotlin/com/rizki/mufrizal/aplikasi/helpdesk/labti/AplikasiHelpDesk.kt
@@ -0,0 +1,22 @@
+package com.rizki.mufrizal.aplikasi.helpdesk.labti
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.hateoas.config.EnableHypermediaSupport
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since 01 March 2016, 14:21
+ * @Project Server
+ * @Package com.rizki.mufrizal.aplikasi.helpdesk.labti
+ */
+
+@SpringBootApplication
+@EnableRedisHttpSession
+@EnableHypermediaSupport(type = arrayOf(EnableHypermediaSupport.HypermediaType.HAL))
+open class AplikasiHelpDesk
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AplikasiHelpDesk::class.java, *args)
+}
