6e8b23042bb9e4fccdcd38bae1253ed29928632b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-03 21:40:16 +0700

    tambah enum status

---
+++ Server/src/main/kotlin/com/rizki/mufrizal/aplikasi/helpdesk/labti/domain/Status.kt
@@ -0,0 +1,11 @@
+package com.rizki.mufrizal.aplikasi.helpdesk.labti.domain
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since 03 March 2016, 21:36
+ * @Project Server
+ * @Package com.rizki.mufrizal.aplikasi.helpdesk.labti.domain
+ */
+enum class Status {
+    selesai, dikerjakan, sedangDikerjakan
+}
